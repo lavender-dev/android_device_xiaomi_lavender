8a9bfd06a8e7936e4d4afac2c9578486fe3e9608
subdragonzj <zj502407679@gmail.com>
2019-11-17 18:02:38 +0800

    Revert "lavender: Bringup AOSiP"
    This reverts commit 0b420655f6f73a1eb042559173609be5e740d89f.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosip_lavender-user \
-    aosip_lavender-userdebug \
-    aosip_lavender-eng
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng
